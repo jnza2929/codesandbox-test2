6d884399587aae79602c490b4f96d1d82c10dac1
jnza2929 <110068020+jnza2929@users.noreply.github.com>
2022-07-31 14:24:51 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+/**
+ * const, let等の変数宣言
+ */
+// var vall = "var変数";
+// console.log(vall);
+
+// // var変数は上書き可能
+// vall = "var変数を上書き";
+// console.log(vall);
+
+// // var変数は再現宣言可能
+// var vall = "var変数を再宣言";
+// console.log(vall);
+
+// let val2 = "lwet変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "letは変数を上書き";
+// console.log(val2);
+
+// letは再宣言不可能
+// let val2 = "let変数を再宣言";
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は再宣言、上書き可能
+// // val3 = "const変数を上書き";
+
+// // 定数をオブジェクトとして宣言する。
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// console.log(val4);
+
+// // constで定義してもオブジェクトの場合は、プロパティを変更できる。
+// val4.name = "jak";
+// console.log(val4);
+
+// 配列を宣言
+// const val5 = ["dog", "cat"];
+// // constで宣言した配列はプロパティの変更が可能
+// val5[0] = "bird";
+// val5.push("monkey");
+
+// console.log(val5);
