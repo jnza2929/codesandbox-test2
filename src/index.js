62e5c69bb38d4ee2a52072cf82120c03cc985fed
jnza2929 <110068020+jnza2929@users.noreply.github.com>
2022-08-01 08:27:44 +0900

    2022/08/01 朝活内容保存

--- src/index.js
+++ src/index.js
@@ -46,3 +46,177 @@
 // val5.push("monkey");
 
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "じゃけぇ";
+// const age = 28;
+// 「私の名前はじゃけぇです。年齢は28歳です。」
+
+// 従来の方法
+// const message = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message);
+
+// テンプレート文字列を用いた方法
+// テンプレート3字列は、逆引用符（shift+@）で囲むこと。
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// アロー関数
+// {}で囲う場合には、returnを記述する必要がある。
+// const func2 = (str) => {
+//   return str;
+// };
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// 変数myProfileに配列型の値を代入
+// const myProfile = [`じゃけぇ`, 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// //sayHello("じゃけぇ");
+// sayHello();
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// // スプレッド構文
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// // スプレッド構文の方が短い、順番に処理を実行させるときは便利
+// sumFunc(...arr1);
+
+// まとめる
+// 配列をまとめる時にもスプレッド構文が使える。
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(...arr3);
+
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// // 配列のコピー
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// // 配列の結合
+// // 結合元の配列には影響が出ない。
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// // コピー元の配列であるarr4もarrr8の変更値で
+// // 上書きされるので注意が必要である。
+// console.log(arr8);
+// console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// 配列に格納された要素分、ループ処理を回して配列内の要素を表示する。
+// for (let index = 0; index < nameArr.length; index++) {
+//   //console.log(nameArr[index]);
+//   console.log(`${index + 1}番目は、${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は、${name}です。`));
+
+// filter：ある条件に合致した情報だけを取り出すことができる。
+// const numArr = [1, 2, 3, 4, 5];
+// const newNameArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNameArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時：条件がfalseのとき
+// const vall = 1 > 0 ? "trueです" : "falseです";
+// console.log(vall);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください。";
+// console.log(formattedNum);
+// console.log("formattedNum");
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲内です。";
+};
+console.log(checkSum(99, 0));
+console.log(checkSum(100, 0));
+console.log(checkSum(101, 0));
